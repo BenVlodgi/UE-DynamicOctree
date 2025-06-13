d914ede9ffe3d314e5d5df0fbe29098e4477a4a1
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2025-06-13 16:00:31 -0400

    Update Plugin details and shrink build.cs

--- Source/DynamicOctreeRuntime/DynamicOctreeRuntime.Build.cs
+++ Source/DynamicOctreeRuntime/DynamicOctreeRuntime.Build.cs
@@ -4,38 +4,22 @@ using UnrealBuildTool;
 
 public class DynamicOctreeRuntime : ModuleRules
 {
-	public DynamicOctreeRuntime(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicIncludePaths.AddRange(new string[]
-		{
-		});
+    public DynamicOctreeRuntime(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+        });
 
-		PrivateIncludePaths.AddRange(new string[]
-		{
-		});
-
-
-		PublicDependencyModuleNames.AddRange(new string[]
-		{
-			"Core",
-		});
-
-
-		PrivateDependencyModuleNames.AddRange(new string[]
-		{
-			"CoreUObject",
-			"Engine",
-			"Slate",
-			"SlateCore",
-			"GeometryCore",
-		});
-
-
-		DynamicallyLoadedModuleNames.AddRange(new string[]
-		{
-		});
-	}
+        PrivateDependencyModuleNames.AddRange(new string[]
+        {
+            "CoreUObject",
+            "Engine",
+            "Slate",
+            "SlateCore",
+            "GeometryCore",
+        });
+    }
 }
